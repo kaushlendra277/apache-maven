941cc05b5164e0f8bde99b5aa143bdf1bd778688
kaushlendra277 <kaushlendra277@gmail.com>
2019-04-22 05:51:22 -0700

    added maven surefire plugin project

---
+++ 6.maven-surefire-plugin/src/main/java/com/ksc/apache/maven/root/MavenDependency.java
@@ -0,0 +1,12 @@
+package com.ksc.apache.maven.root;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class MavenDependency {
+
+	public static void main(String[] args) {
+		String str = "Hello World";
+		System.out.println(StringUtils.upperCase(str));
+	}
+
+}
---
+++ 6.maven-surefire-plugin/src/test/java/HelloWorldTest.java
@@ -0,0 +1,11 @@
+
+public class HelloWorldTest {
+
+	public void HelloTest() {
+		System.out.println("*************TEST- Hello");
+	}
+	
+	public void testWorld() {
+		System.out.println("*************TEST- World");
+	}
+}
