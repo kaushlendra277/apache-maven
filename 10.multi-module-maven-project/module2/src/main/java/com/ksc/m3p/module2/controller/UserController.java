7537626d3826d49bd665ff74e8fded0d96b990b7
kaushlendra277 <kaushlendra277@gmail.com>
2019-04-23 01:17:48 -0700

    added multi module maven project-module 2

---
+++ 10.multi-module-maven-project/module2/src/main/java/com/ksc/m3p/module2/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.ksc.m3p.module2.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.m3p.module1.entities.User;
+
+public class UserController {
+
+	public List<User> getAll(){
+		return new ArrayList<User>();
+	}
+}
