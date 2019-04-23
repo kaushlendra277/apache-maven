3f8ad346c0eeb4a0dee076530050f9eb71d66e0f
kaushlendra277 <kaushlendra277@gmail.com>
2019-04-23 00:48:43 -0700

    added multi module maven project

---
+++ 10.multi-module-maven-project/module1/src/main/java/com/m3p/module1/entities/User.java
@@ -0,0 +1,20 @@
+package com.m3p.module1.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class User {
+	@Id
+	private String id;
+	private String name;
+}
